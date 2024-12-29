b20b1b951b8b93c54d67558c235331e4d141c212
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2024-12-30 01:39:48 +0330

    Define melody and note durations

--- src/noteGeneretion.py
+++ src/noteGeneretion.py
@@ -9,5 +9,13 @@ frequencies = [
     16.352, 17.324, 18.354, 19.445, 20.602, 21.827, 23.125,
     24.500, 25.957, 27.500, 29.135, 30.868
 ]  # Frequencies of notes in Hz
-
 print("Base structure set up for generating musical notes.")
+
+
+# Example melody and durations
+melody = [27.500, 30.868, 25.957, 24.500, 27.500]
+durations = [0.5, 0.5, 0.5, 0.5, 0.5]
+silence_duration = 0.025
+
+print("Melody and durations defined.")
+
