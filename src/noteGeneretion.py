7545306bc7d64ce70ddaa398cc81a6e8b879092f
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2024-12-30 16:13:43 +0330

    Parse Harry Potter notes and calculate frequencies

--- src/noteGeneretion.py
+++ src/noteGeneretion.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.io.wavfile import write
 
 
-
+fs = 44100  
 # Base frequencies for notes 
 note_frequencies = {
     'C': 261.63, 'C#': 277.18, 'D': 293.66, 'D#': 311.13, 'E': 329.63,
@@ -10,3 +10,34 @@ note_frequencies = {
     'A#': 466.16, 'B': 493.88
 }
 
+# frequency for a given note and octave
+def get_frequency(note, octave):
+    base_freq = note_frequencies[note]
+    return base_freq * (2 ** (octave - 4))
+
+# Define Harry Potter notes
+noteHarryPotter = [
+    'B 4 0.3', 'E 5 0.6', 'G 5 0.2', 'F# 5 0.3', 'E 5 0.6',
+    'B 5 0.4', 'A 5 0.8', 'F# 5 0.8', 'E 5 0.6', 'G 5 0.2',
+    'F# 5 0.3', 'D# 5 0.7', 'F 5 0.4', 'B 4 1.6', 'B 4 0.3',
+    'E 5 0.6', 'G 5 0.2', 'F# 5 0.3', 'E 5 0.6', 'B 5 0.4',
+    'D 6 0.6', 'C# 6 0.3', 'C 6 0.6', 'G# 5 0.3', 'C 5 0.5',
+    'B 5 0.2', 'A# 5 0.3', 'A# 4 0.6', 'G 5 0.3', 'E 5 1.6',
+    'G 5 0.3', 'B 5 0.6', 'G 5 0.3', 'B 5 0.6', 'G 5 0.3',
+    'C 6 0.6', 'B 5 0.3', 'A# 5 0.6', 'F# 5 0.3', 'G 5 0.5',
+    'B 5 0.2', 'A# 5 0.3', 'A# 4 0.6', 'B 4 0.4', 'B 5 1.6',
+    'G 5 0.3', 'B 5 0.7', 'G 5 0.3', 'B 5 0.7', 'G 5 0.3',
+    'D 6 0.7', 'C# 6 0.3', 'C 6 0.8', 'G# 5 0.3', 'C 6 0.6',
+    'B 5 0.2', 'A# 5 0.3', 'A# 4 0.6', 'G 5 0.4', 'E 5 1', 'E 5 1.6'
+]
+
+# Parse notes and calculate frequencies
+melody = []
+durations = []
+
+for note_entry in noteHarryPotter:
+    note, octave, duration = note_entry.split()
+    melody.append(get_frequency(note, int(octave)))
+    durations.append(float(duration))
+
+print("Parsed notes and calculated frequencies DONE!.")
