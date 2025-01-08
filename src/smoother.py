9a4117149d3e2ca191a5e5757214aeb172e382e5
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-01-08 18:08:33 +0330

    Perform fourier transform

--- src/smoother.py
+++ src/smoother.py
@@ -17,3 +17,21 @@ for file in sorted(os.listdir(directory)):
         # Normalize the signal
         signal = signal / np.max(np.abs(signal))
 
+        # Perform Fourier Transform
+        fft_spectrum = np.fft.fft(signal)
+        frequencies = np.fft.fftfreq(len(fft_spectrum), 1 / fs)
+        magnitude = np.abs(fft_spectrum)
+
+        # Limit to positive frequencies
+        positive_freqs = frequencies[:len(frequencies) // 2]
+        positive_magnitude = magnitude[:len(magnitude) // 2]
+
+        # Plot the Fourier spectrum
+        plt.figure(figsize=(10, 6))
+        plt.plot(positive_freqs, positive_magnitude, label=f'{note_name} Spectrum')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Magnitude')
+        plt.title(f'Fourier Transform of {note_name}')
+        plt.grid()
+        plt.legend()
+        plt.show()
