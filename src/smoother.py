7d8c55c96d64385b2af0e888acadf9eab70b191b
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-01-30 16:01:14 +0330

    Implemented analyze_audio function to extract harmonic frequencies

--- src/smoother.py
+++ src/smoother.py
@@ -1,39 +1,22 @@
 import os
-from scipy.io.wavfile import read
 import numpy as np
+import soundfile as sf
+import scipy.fftpack as fft
 import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.signal import chirp
 
 
-directory = '../Audio/Octave5'
-harmonics_data = {} 
-
-
-for file in sorted(os.listdir(directory)):
-    if file.endswith('.wav'):
-        note_name = file.split('.')[0]
-
-        fs, signal = read(os.path.join(directory, file))
-
-        signal = signal / np.max(np.abs(signal))
-
-        fft_spectrum = np.fft.fft(signal)
-        frequencies = np.fft.fftfreq(len(fft_spectrum), 1 / fs)
-        magnitude = np.abs(fft_spectrum)
-
-        # Limit to positive frequencies
-        positive_freqs = frequencies[:len(frequencies) // 2]
-        positive_magnitude = magnitude[:len(magnitude) // 2]
-
-        # plt.figure(figsize=(10, 6))
-        # plt.plot(positive_freqs, positive_magnitude, label=f'{note_name} Spectrum')
-        # plt.xlabel('Frequency (Hz)')
-        # plt.ylabel('Magnitude')
-        # plt.title(f'Fourier Transform of {note_name}')
-        # plt.grid()
-        # plt.legend()
-        # plt.show()
-
-        peaks_indices = positive_magnitude.argsort()[-7:][::-1]  # Top 7 including DC
-        peaks_indices = [idx for idx in peaks_indices if positive_freqs[idx] > 0][:6]  # Exclude DC
-
-        harmonics_data[note_name] = [(positive_freqs[idx], positive_magnitude[idx]) for idx in peaks_indices]
+def analyze_audio(file_path):
+    data, samplerate = sf.read(file_path)
+    if len(data.shape) > 1:
+        data = np.mean(data, axis=1)
+    spectrum = np.abs(fft.fft(data))
+    freqs = fft.fftfreq(len(data), 1/samplerate)
+    pos_mask = freqs > 0
+    freqs = freqs[pos_mask]
+    spectrum = spectrum[pos_mask]
+    top_indices = np.argsort(spectrum)[-6:][::-1]
+    top_freqs = freqs[top_indices]
+    top_values = spectrum[top_indices]
+    return samplerate, top_freqs, top_values
