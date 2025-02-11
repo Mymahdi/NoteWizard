b3b4f38582dcc7dfc98ac3eec0b15cd8fcda768c
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-02-11 11:55:23 +0330

    Define vector variables in MATLAB script

--- ca1/shortProblems.m
+++ ca1/shortProblems.m
@@ -9,3 +9,18 @@ c = 2 + 3i;
 d = exp(2*pi/3);
 
 
+%% Step 2: Define Vector Variables
+% aVec = [3.14, 15, 9, 26]
+aVec = [3.14, 15, 9, 26];
+
+% bVec is a column vector
+bVec = [2.71; 8; 28; 182];
+
+% cVec: All numbers from 5 to -5 in increments of -0.2
+cVec = 5:-0.2:-5;
+
+% dVec: Logarithmically spaced numbers between 10^0 and 10^1, ensuring correct length
+dVec = logspace(0, 1, 100);
+
+% eVec: String vector
+eVec = 'Hello';
