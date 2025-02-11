fb097da768a9a51d7aa6649191262d130a57415e
Mymahdi <henry15ronaldo79carlo1@gmail.com>
2025-02-11 11:48:27 +0330

    Define scalar variables a, b, c, d in MATLAB

---
+++ ca1/shortProblems.m
@@ -0,0 +1,11 @@
+
+%% Step 1: Define Scalar Variables
+a = 10;
+% b = 2.5 * 10^23
+b = 2.5e23;
+% c = 2 + 3i (complex number)
+c = 2 + 3i;
+% d = e^(2*pi/3), where e is Euler's number
+d = exp(2*pi/3);
+
+
